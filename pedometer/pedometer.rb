91826912385f0a171a54b8a4f7021f7850fc6ead
Dessy Daskalov <dessy.daskalov@gmail.com>
2014-03-05 00:06:32 -0500

    Rename to method 1 and method 2

--- pedometer/pedometer.rb
+++ pedometer/pedometer.rb
@@ -7,56 +7,56 @@ include FileUtils::Verbose
 
 get '/test' do
   begin
-    data_a = File.read('public/test/accelerometer.txt')
-    data_g = File.read('public/test/gravity.txt')
+    data_1 = File.read('public/test/accelerometer.txt')
+    data_2 = File.read('public/test/gravity.txt')
 
-    user_a     = User.new(:gender => 'female', :height => 167)
-    @device_a   = Device.new(:data => data_a, :rate => 100)
-    @parser_a   = Parser.new(@device_a)
-    @analyzer_a = Analyzer.new(@parser_a, user_a)
-    @analyzer_a.measure
+    user_1      = User.new(:gender => 'female', :height => 167)
+    @device_1   = Device.new(:data => data_1, :rate => 100)
+    @parser_1   = Parser.new(@device_1)
+    @analyzer_1 = Analyzer.new(@parser_1, user_1)
+    @analyzer_1.measure
 
-    user_g     = User.new(:gender => 'female', :height => 167)
-    @device_g   = Device.new(:data => data_g, :rate => 100)
-    @parser_g   = Parser.new(@device_g)
-    @analyzer_g = Analyzer.new(@parser_g, user_g)
-    @analyzer_g.measure    
+    user_2      = User.new(:gender => 'female', :height => 167)
+    @device_2   = Device.new(:data => data_2, :rate => 100)
+    @parser_2   = Parser.new(@device_2)
+    @analyzer_2 = Analyzer.new(@parser_2, user_2)
+    @analyzer_2.measure
 
-    @x_a = @parser_a.parsed_data.collect { |d| d[:x] }
-    @y_a = @parser_a.parsed_data.collect { |d| d[:y] }
-    @z_a = @parser_a.parsed_data.collect { |d| d[:z] }
+    @x_1 = @parser_1.parsed_data.collect { |d| d[:x] }
+    @y_1 = @parser_1.parsed_data.collect { |d| d[:y] }
+    @z_1 = @parser_1.parsed_data.collect { |d| d[:z] }
 
-    @x_g = @parser_g.parsed_data.collect { |d| d[:x] }
-    @y_g = @parser_g.parsed_data.collect { |d| d[:y] }
-    @z_g = @parser_g.parsed_data.collect { |d| d[:z] }
+    @x_2 = @parser_2.parsed_data.collect { |d| d[:x] }
+    @y_2 = @parser_2.parsed_data.collect { |d| d[:y] }
+    @z_2 = @parser_2.parsed_data.collect { |d| d[:z] }
 
-    @xg_a = @parser_a.parsed_data.collect { |d| d[:xg] }
-    @yg_a = @parser_a.parsed_data.collect { |d| d[:yg] }
-    @zg_a = @parser_a.parsed_data.collect { |d| d[:zg] }
+    @xg_1 = @parser_1.parsed_data.collect { |d| d[:xg] }
+    @yg_1 = @parser_1.parsed_data.collect { |d| d[:yg] }
+    @zg_1 = @parser_1.parsed_data.collect { |d| d[:zg] }
 
-    @xg_g = @parser_g.parsed_data.collect { |d| d[:xg] }
-    @yg_g = @parser_g.parsed_data.collect { |d| d[:yg] }
-    @zg_g = @parser_g.parsed_data.collect { |d| d[:zg] }
+    @xg_2 = @parser_2.parsed_data.collect { |d| d[:xg] }
+    @yg_2 = @parser_2.parsed_data.collect { |d| d[:yg] }
+    @zg_2 = @parser_2.parsed_data.collect { |d| d[:zg] }
 
-    @xyz_a = @parser_a.parsed_data.collect { |d| Math.sqrt((d[:x]*d[:x])+(d[:y]*d[:y])+(d[:z]*d[:z])) }
-    @xyz_g = @parser_g.parsed_data.collect { |d| Math.sqrt((d[:x]*d[:x])+(d[:y]*d[:y])+(d[:z]*d[:z])) }
+    @xyz_1 = @parser_1.parsed_data.collect { |d| Math.sqrt((d[:x]*d[:x])+(d[:y]*d[:y])+(d[:z]*d[:z])) }
+    @xyz_2 = @parser_2.parsed_data.collect { |d| Math.sqrt((d[:x]*d[:x])+(d[:y]*d[:y])+(d[:z]*d[:z])) }
 
-    @xyzg_a = @parser_a.parsed_data.collect { |d| Math.sqrt((d[:xg]*d[:xg])+(d[:yg]*d[:yg])+(d[:zg]*d[:zg])) }
-    @xyzg_g = @parser_g.parsed_data.collect { |d| Math.sqrt((d[:xg]*d[:xg])+(d[:yg]*d[:yg])+(d[:zg]*d[:zg])) }
+    @xyzg_1 = @parser_1.parsed_data.collect { |d| Math.sqrt((d[:xg]*d[:xg])+(d[:yg]*d[:yg])+(d[:zg]*d[:zg])) }
+    @xyzg_2 = @parser_2.parsed_data.collect { |d| Math.sqrt((d[:xg]*d[:xg])+(d[:yg]*d[:yg])+(d[:zg]*d[:zg])) }
 
 
     # Raw total acceleration in each of x, y, z (from accelerometer)
-    @x_raw_a = @device_a.data.split(';').inject([]) {|a, data| a << data.split(',')[0].to_f }
-    @y_raw_a = @device_a.data.split(';').inject([]) {|a, data| a << data.split(',')[1].to_f }
-    @z_raw_a = @device_a.data.split(';').inject([]) {|a, data| a << data.split(',')[2].to_f }
+    @x_raw_1 = @device_1.data.split(';').inject([]) {|a, data| a << data.split(',')[0].to_f }
+    @y_raw_1 = @device_1.data.split(';').inject([]) {|a, data| a << data.split(',')[1].to_f }
+    @z_raw_1 = @device_1.data.split(';').inject([]) {|a, data| a << data.split(',')[2].to_f }
 
     # Total acceleration in each of x, y, z (calculated by x + xg, y + yg, z + zg from device motion)
-    @x_plus_xg_g = @parser_g.parsed_data.collect { |d| d[:x] + d[:xg] }
-    @y_plus_yg_g = @parser_g.parsed_data.collect { |d| d[:y] + d[:yg] }
-    @z_plus_zg_g = @parser_g.parsed_data.collect { |d| d[:z] + d[:zg] }
+    @x_plus_xg_2 = @parser_2.parsed_data.collect { |d| d[:x] + d[:xg] }
+    @y_plus_yg_2 = @parser_2.parsed_data.collect { |d| d[:y] + d[:yg] }
+    @z_plus_zg_2 = @parser_2.parsed_data.collect { |d| d[:z] + d[:zg] }
 
     # Magnitude of total acceleration (from accelerometer)
-    @magnitude_total_a = @device_a.data.split(';').inject([]) do |a, data| 
+    @magnitude_total_1 = @device_1.data.split(';').inject([]) do |a, data| 
       x = data.split(',')[0].to_f
       y = data.split(',')[1].to_f
       z = data.split(',')[2].to_f
@@ -64,7 +64,7 @@ get '/test' do
     end
 
     # Magnitude of total acceleration (calculated by x + xg, y + yg, z + zg from device motion)
-    @magnitude_total_g = @parser_g.parsed_data.collect do |d| 
+    @magnitude_total_2 = @parser_2.parsed_data.collect do |d| 
       x = d[:x] + d[:xg]
       y = d[:y] + d[:yg]
       z = d[:z] + d[:zg]
